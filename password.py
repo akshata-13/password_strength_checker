741e897b35606706de0f82986d5727c3a0c53071
Akshata <142037798+akshata-13@users.noreply.github.com>
2024-05-30 18:24:01 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,22 @@
+import re
+
+def valid_password(password):
+    if len(password) < 8:
+        return False
+    if not re.search(r'[A-Z]', password):
+        return False
+    if not re.search(r'[a-z]', password):
+        return False
+    if not re.search(r'\d', password):
+        return False
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        return False
+    return True
+
+password = input("Input your password: ")
+is_valid = valid_password(password)
+
+if is_valid:
+    print("Valid Password.")
+else:
+    print("Password does not meet requirements.")
